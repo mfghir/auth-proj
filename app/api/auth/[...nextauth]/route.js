52ec86cca4d04f18f5b9ab713fff16f22f1e618f
mfghir <fatemeghafari77@gmail.com>
2023-12-11 00:41:50 -0800

    test 2

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -21,54 +21,54 @@ export const authOptions = {
     }),
   ],
 
-  callbacks: {
-    async signIn({ user, account }) {
-      if (account.provider === "google") {
-        const { name, email } = user;
-        try {
-          await connectMongoDB();
-          const userExists = await User.findOne({ email });
+  // callbacks: {
+  //   async signIn({ user, account }) {
+  //     if (account.provider === "google") {
+  //       const { name, email } = user;
+  //       try {
+  //         await connectMongoDB();
+  //         const userExists = await User.findOne({ email });
 
-          if (!userExists) {
-            const res = await fetch("/api/user", {
-              method: "POST",
-              headers: { "Content-Type": "application/json" },
-              body: JSON.stringify({ name, email }),
-            });
+  //         if (!userExists) {
+  //           const res = await fetch("/api/user", {
+  //             method: "POST",
+  //             headers: { "Content-Type": "application/json" },
+  //             body: JSON.stringify({ name, email }),
+  //           });
 
-            if (res.ok) {
-              return user;
-            }
-          }
-        } catch (error) {
-          console.log(error);
-        }
-      }
-      if (account.provider === "github") {
-        const { name, email } = user;
-        try {
-          await connectMongoDB();
-          const userExists = await User.findOne({ email });
+  //           if (res.ok) {
+  //             return user;
+  //           }
+  //         }
+  //       } catch (error) {
+  //         console.log(error);
+  //       }
+  //     }
+  //     if (account.provider === "github") {
+  //       const { name, email } = user;
+  //       try {
+  //         await connectMongoDB();
+  //         const userExists = await User.findOne({ email });
 
-          if (!userExists) {
-            const res = await fetch("/api/user", {
-              method: "POST",
-              headers: { "Content-Type": "application/json" },
-              body: JSON.stringify({ name, email }),
-            });
+  //         if (!userExists) {
+  //           const res = await fetch("/api/user", {
+  //             method: "POST",
+  //             headers: { "Content-Type": "application/json" },
+  //             body: JSON.stringify({ name, email }),
+  //           });
 
-            if (res.ok) {
-              return user;
-            }
-          }
-        } catch (error) {
-          console.log(error);
-        }
-      }
+  //           if (res.ok) {
+  //             return user;
+  //           }
+  //         }
+  //       } catch (error) {
+  //         console.log(error);
+  //       }
+  //     }
 
-      return user;
-    },
-  },
+  //     return user;
+  //   },
+  // },
 };
 
 const handler = NextAuth(authOptions);
--- models/User.js
+++ models/User.js
@@ -1,18 +1,18 @@
-import mongoose, { Schema, models } from "mongoose";
+// import mongoose, { Schema, models } from "mongoose";
 
-const userSchema = new Schema(
-  {
-    email: {
-      type: String,
-      required: true,
-    },
-    name: {
-      type: String,
-      required: true,
-    },
-  },
-  { timestamps: true }
-);
+// const userSchema = new Schema(
+//   {
+//     email: {
+//       type: String,
+//       required: true,
+//     },
+//     name: {
+//       type: String,
+//       required: true,
+//     },
+//   },
+//   { timestamps: true }
+// );
 
-const User = models.User || mongoose.model("User", userSchema);
-export default User;
+// const User = models.User || mongoose.model("User", userSchema);
+// export default User;
